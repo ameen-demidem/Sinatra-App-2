9ca22e24026d85497f5777e96c6b52fa6b075429
Ameen Demidem <ameen.demidem@gmail.com>
2016-08-08 17:25:02 -0400

    Add a view to post a new song

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,14 @@ get "/songs" do
   @songs = Song.all
   erb :"songs/index"
 end
+
+get "/songs/new" do
+  @song = Song.new(title: "title", author: "author")
+  erb :"songs/new"
+end
+
+post "/songs" do
+  puts params
+  @song = Song.create(params)
+  @song.persisted? ? redirect("/songs") : erb(:"songs/new")
+end
